906935ffabcfe2c4d6eae132a63758e5c92876c6
Pierluigi Trocini <59370899+pierluigiTrocini@users.noreply.github.com>
2023-06-05 10:25:38 +0200

    frontend: android: modify user profile Activity

--- app/src/main/java/com/trenicalea/trintedapp/UserProfileActivity.kt
+++ app/src/main/java/com/trenicalea/trintedapp/UserProfileActivity.kt
@@ -2,21 +2,28 @@ package com.trenicalea.trintedapp
 
 
 import android.graphics.Bitmap
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.BrokenImage
+import androidx.compose.material.icons.filled.CalendarToday
 import androidx.compose.material.icons.filled.HideImage
 import androidx.compose.material.icons.filled.Mail
 import androidx.compose.material.icons.filled.Person
@@ -24,6 +31,7 @@ import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Divider
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -37,6 +45,7 @@ import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -59,18 +68,19 @@ fun UserProfileActivity(
         false
     )
 ) {
-    val purchasesPagerState = rememberPagerState(initialPage = 1)
-    val purchasesList = listOf<OrdineDto>()
-
-    val salesPagerState = rememberPagerState(initialPage = 1)
-    val salesList = listOf<OrdineDto>(
+    val purchasesList = listOf<OrdineDto>(
         OrdineDto(1, LocalDate.now()),
         OrdineDto(2, LocalDate.now()),
         OrdineDto(3, LocalDate.now()),
         OrdineDto(4, LocalDate.now())
     )
 
-
+    val salesList = listOf<OrdineDto>(
+        OrdineDto(1, LocalDate.now()),
+        OrdineDto(2, LocalDate.now()),
+        OrdineDto(3, LocalDate.now()),
+        OrdineDto(4, LocalDate.now())
+    )
 
     Card(
         elevation = CardDefaults.cardElevation(defaultElevation = 5.dp),
@@ -131,39 +141,78 @@ fun UserProfileActivity(
                     )
                 }
             }
-            Row(
-                horizontalArrangement = Arrangement.Start,
-                verticalAlignment = Alignment.CenterVertically,
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(10.dp)
-            ) {
 
-            }
             Divider()
 
-            Row(
-                horizontalArrangement = Arrangement.Start,
-                verticalAlignment = Alignment.CenterVertically,
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(10.dp)
-            ) {
+            Carousel(list = salesList, title = stringResource(R.string.recentSales))
 
-                Text(text = stringResource(id = R.string.recentSales))
+            Divider()
 
-            }
-        }
+            Carousel(list = purchasesList, title = stringResource(R.string.recentPurchases))
 
-        Row(
-            horizontalArrangement = Arrangement.Start,
-            verticalAlignment = Alignment.CenterVertically,
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(10.dp)
-        ) {
-            Text(text = stringResource(id = R.string.recentPurchases))
+
         }
     }
-
 }
+
+@Composable
+fun Carousel(list: List<OrdineDto>, title: String) {
+    Row(
+        horizontalArrangement = Arrangement.Start,
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(10.dp)
+    ) {
+        Text(
+            text = title,
+            fontWeight = FontWeight.Bold,
+            fontSize = 20.sp
+        )
+    }
+    Row(
+        horizontalArrangement = Arrangement.Start,
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(10.dp)
+    ) {
+        LazyRow {
+            items(list) { order ->
+                Card(
+                    modifier = Modifier
+                        .padding(8.dp)
+                        .wrapContentHeight()
+                        .wrapContentWidth()
+                ) {
+                    Column(horizontalAlignment = Alignment.CenterHorizontally,
+                        verticalArrangement = Arrangement.Center) {
+                        Row() {
+                            val orderImage: Bitmap? = null
+                            if (orderImage == null) {
+                                Icon(
+                                    imageVector = Icons.Filled.HideImage,
+                                    contentDescription = stringResource(id = R.string.noImages),
+                                    modifier = Modifier.size(120.dp)
+                                )
+                            } else {
+
+                            }
+                        }
+                        Row() {
+                            Icon(
+                                imageVector = Icons.Filled.CalendarToday,
+                                contentDescription = null,
+                                modifier = Modifier.size(20.dp)
+                            )
+                            Text(text = "${order.dataAcquisto}")
+                        }
+                    }
+
+                }
+            }
+        }
+    }
+}
+
+
